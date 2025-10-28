144e734ec637ec2b9800de59e128240ce6989391
AN MIN YONG <iks05280204@gmail.com>
2025-10-28 15:25:37 +0900

    Initial commit

---
+++ game_world.py
@@ -0,0 +1,23 @@
+world = [[],[]]
+
+#월드에 객체 추가
+def add_object(o, depth = 0):
+    world[depth].append(o)
+
+def add_objects(ol, depth = 0):
+    world[depth] += ol
+
+def remove_object(o):
+    for layer in world:
+        if o in layer:
+            layer.remove(o)
+            return
+def update():
+    for layer in world:
+        for o in layer:
+            o.update()
+
+def render():
+    for layer in world:
+        for o in layer:
+            o.draw()
